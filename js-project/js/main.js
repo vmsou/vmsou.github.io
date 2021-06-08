b3c599353652566228cef85cc38e827ea1644b3f
vmsou <viniciusmarcs@icloud.com>
2021-06-08 17:38:39 -0300

    project moved

---
+++ js-project/js/main.js
@@ -0,0 +1,26 @@
+function activeElement() {
+    let activeHref = window.location.href;
+    let container = document.getElementsByClassName("navLink");
+    let e;
+    for (let i = 0; i < container.length; i++) {
+        e = container[i];
+        if (e.href === activeHref) {
+            e.classList.add("active");
+            return;
+        }
+    }
+}
+
+
+function loadTemplate(loc, tagName="header") {
+    let header = document.createElement(tagName);
+    document.body.append(header);
+    $(tagName).load(loc, null, function () {
+        $("#homeLogo").click(function () {
+            window.location = "https://vmsou.github.io/";
+        })
+        activeElement();
+    });
+
+}
+
