39fafd9c2c9d9ffd71a35c11473dccf2782cb51e
vmsou <viniciusmarcs@icloud.com>
2021-06-10 15:09:56 -0300

    [PHP] Steganography implemented

--- js-project/php/loginSave.php
+++ js-project/php/loginSave.php
@@ -2,7 +2,18 @@
     $user = $_POST["ch_user"];
     $password = $_POST["ch_pass"];
 
-    if ($user == "admin" && $password == "e8d95a51f3af4a3b134bf6bb680a213a") {
+    $text = file_get_contents("Ad8fak.txt");
+    $text_len = strlen($text);
+
+    $usuario = "";
+    for ($i = 0; $i < $text_len; $i++)  {
+        $currLetter = $text[$i];
+        if ($currLetter == '.' || $currLetter == ',') {
+            $usuario .= $text[$i + 1];
+        }
+    }
+
+    if ($user == $usuario && $password == "e8d95a51f3af4a3b134bf6bb680a213a") {
         $retorno = utf8_encode("é admin");
     } else {
         $retorno = utf8_encode("não é admin");
