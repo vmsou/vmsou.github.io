c8da1e0a27d44284ed6918bc6521eccd9d44b647
vmsou <viniciusmarcs@icloud.com>
2021-06-15 13:04:29 -0300

    [JS] GET data from JSON

--- js-project/js/videos.js
+++ js-project/js/videos.js
@@ -1,48 +1,48 @@
-let videos_links = [
-    "3WsyBosVTlk",
-    "ASzOzrB-a9E",
-    "Wr9Add371is",
-    "wDZ-9B6hX5A",
-    "asqWq8MIAWA",
-]
-
-let videos_names = [
-    "RIO: Raised in Oblivion",
-    "Battlefield 2042",
-    "Avatar: Frontiers of Pandora",
-    "Halo Infinite",
-    "Elden Ring",
-]
+let videos_links = [], videos_names = [];
 
 function addVideos() {
     let container = document.getElementById("videos-container");
-    for (let i = 0; i < videos_links.length; i++) {
-        let e = document.createElement("div");
-        let img = document.createElement("img");
-        let modal = document.getElementById("showcase");
-        let video_modal = document.getElementById("showcase-video");
-        let caption = document.getElementById("caption");
+    let requestURL = "../json/trailerData.json";
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
 
-        e.className = "box";
-
-        img.src = "https://i3.ytimg.com/vi/" + videos_links[i] + "/maxresdefault.jpg";
-        img.className = "box-image";
-        img.alt = videos_names[i];
-        img.onclick = function() {
-            modal.style.display = "block";
-            video_modal.src = "https://www.youtube.com/embed/" + videos_links[i];
-            caption.innerHTML = videos_names[i];
+    request.onload = function() {
+        let data = request.response;
+        for (let i = 0; i < data.length; i++) {
+            videos_links.push(data[i]["videoLink"]);
+            videos_names.push(data[i]["trailerName"]);
         }
 
-        modal.onclick = function() {
-            modal.style.display = "none";
-            video_modal.src = "";
-            caption.innerHTML = "";
+        for (let i = 0; i < videos_links.length; i++) {
+            let e = document.createElement("div");
+            let img = document.createElement("img");
+            let modal = document.getElementById("showcase");
+            let video_modal = document.getElementById("showcase-video");
+            let caption = document.getElementById("caption");
 
+            e.className = "box";
+
+            img.src = "https://i3.ytimg.com/vi/" + videos_links[i] + "/maxresdefault.jpg";
+            img.className = "box-image";
+            img.alt = videos_names[i];
+            img.onclick = function() {
+                modal.style.display = "block";
+                video_modal.src = "https://www.youtube.com/embed/" + videos_links[i];
+                caption.innerHTML = videos_names[i];
+            }
+
+            modal.onclick = function() {
+                modal.style.display = "none";
+                video_modal.src = "";
+                caption.innerHTML = "";
+
+            }
+
+            e.append(img);
+            container.append(e);
         }
-
-        e.append(img);
-        container.append(e);
     }
 }
 
