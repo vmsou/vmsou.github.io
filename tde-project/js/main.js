fea2eaa79a416c9a3d175be9726e5bd966f4c293
vmsou <viniciusmarcs@icloud.com>
2021-06-19 12:26:27 -0300

    [HTML + CSS + JS] Content box visual.

--- tde-project/js/main.js
+++ tde-project/js/main.js
@@ -1,16 +1,22 @@
 let i = 0;
 const imgBox = document.querySelector(".imgBox");
+const contentBox = document.querySelector(".contentBox");
+const boxes = contentBox.getElementsByTagName("div");
 const slides = imgBox.getElementsByTagName("img");
 
 
 function nextSlide() {
-    slides[i++].classList.remove("active");
-    i %= slides.length;
+    slides[i].classList.remove("active");
+    boxes[i].classList.remove("active");
+    i = (i + 1) % slides.length;
     slides[i].classList.add("active");
+    boxes[i].classList.add("active");
 }
 
 function backSlide() {
-    slides[i--].classList.remove("active");
-    if (i < 0) i = slides.length - 1;
+    slides[i].classList.remove("active");
+    boxes[i].classList.remove("active");
+    i = (i - 1 + slides.length) % slides.length;
     slides[i].classList.add("active");
+    boxes[i].classList.add("active");
 }
